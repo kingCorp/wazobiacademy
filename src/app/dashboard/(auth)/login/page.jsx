c17808080467e103cf4a322f94811aa326a27b6f
Kingsley <attasiemj@gmail.com>
2024-04-12 00:27:46 +0100

    fix: page erro catching suspense

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -4,6 +4,8 @@ import styles from "./page.module.css";
 import { getProviders, signIn, useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
+import { Suspense } from 'react'
+
 
 const Login = () => {
   const session = useSession();
@@ -37,45 +39,46 @@ const Login = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <Suspense>
+      <div className={styles.container}>
 
 
-      <div className={styles.body1}>
-        {/* <p>Welcome to WazobiAcademy</p> */}
-        {/* <Image
+        <div className={styles.body1}>
+          {/* <p>Welcome to WazobiAcademy</p> */}
+          {/* <Image
           src={SignupImage}
           layout="fill"
           objectFit="cover"
           quality={100}
           className={styles.signupimg}
         /> */}
-      </div>
-      <div className={styles.body}>
-        <div>
-          <p className={styles.title}>{success ? success : "Welcome Back"}</p>
-          <p className={styles.subtitle}>Please sign in to see the dashboard.</p>
+        </div>
+        <div className={styles.body}>
+          <div>
+            <p className={styles.title}>{success ? success : "Welcome Back"}</p>
+            <p className={styles.subtitle}>Please sign in to see the dashboard.</p>
 
-          <form onSubmit={handleSubmit} className={styles.form}>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              placeholder="Email"
-              required
-              className={styles.input}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              className={styles.input}
-            />
-            <button className={styles.button}>Sign in</button>
-            {error && "Something went wrong!"}
-          </form>
-          <p className={styles.or}>- or -</p>
+            <form onSubmit={handleSubmit} className={styles.form}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                className={styles.input}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                className={styles.input}
+              />
+              <button className={styles.button}>Sign in</button>
+              {error && "Something went wrong!"}
+            </form>
+            <p className={styles.or}>- or -</p>
 
-          {/* <button
+            {/* <button
             onClick={() => {
               signIn("google");
             }}
@@ -83,12 +86,13 @@ const Login = () => {
           >
             Login with Google
           </button> */}
-          <Link className={styles.loginlink} href="/dashboard/register">
-            Create a account <span style={{ color: '#009B77' }}>Signup</span>
-          </Link>
+            <Link className={styles.loginlink} href="/dashboard/register">
+              Create a account <span style={{ color: '#009B77' }}>Signup</span>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
