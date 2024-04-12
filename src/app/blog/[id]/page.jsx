2222d2b44d619512965d2e525511b0fd7cbdbe96
Kingsley <attasiemj@gmail.com>
2024-04-13 00:06:08 +0100

    fix: remove fetch data blog blog id

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,33 +3,33 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-    cache: "no-store",
-  });
+// async function getData(id) {
+//   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    return notFound()
-  }
+//   if (!res.ok) {
+//     return notFound()
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 
-export async function generateMetadata({ params }) {
+// export async function generateMetadata({ params }) {
 
-  const post = await getData(params.id)
-  return {
-    title: post.title,
-    description: post.desc,
-  };
-}
+//   const post = await getData(params.id)
+//   return {
+//     title: post.title,
+//     description: post.desc,
+//   };
+// }
 
 const BlogPost = async ({ params }) => {
-  const data = await getData(params.id);
+  // const data = await getData(params?.id);
   return (
     <div className={styles.container}>
-      <div className={styles.top}>
+      {/* <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
@@ -59,7 +59,7 @@ const BlogPost = async ({ params }) => {
         <p className={styles.text}>
          {data.content}
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
